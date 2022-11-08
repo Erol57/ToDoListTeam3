25478c0cfcb40eeff47c2e26b35a597e92ff64ce
Trygve Horn <trygvehorn@gmail.com>
2022-11-08 19:33:26 +0100

    Color Change on mouseover

---
+++ src/jscript.js
@@ -0,0 +1,5 @@
+function setHamCol(color) {
+    document.getElementById('hamStripe1').style.backgroundColor = color;
+    document.getElementById('hamStripe2').style.backgroundColor = color;
+    document.getElementById('hamStripe3').style.backgroundColor = color;
+}
