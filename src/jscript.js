98d727fd52090e50f264be6a797b6278b56b0d2f
Trygve Horn <trygvehorn@gmail.com>
2022-11-08 19:53:36 +0100

    hamburger Click Enabled

--- src/jscript.js
+++ src/jscript.js
@@ -3,3 +3,7 @@ function setHamCol(color) {
     document.getElementById('hamStripe2').style.backgroundColor = color;
     document.getElementById('hamStripe3').style.backgroundColor = color;
 }
+
+function hamClick () {
+    console.log('click');
+}
