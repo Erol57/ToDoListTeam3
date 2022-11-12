4360d8ab0fbbefd130bd70b6242bfa449387dd51
Trygve Horn <trygvehorn@gmail.com>
2022-11-12 16:37:53 +0100

    ss

--- src/jscript.js
+++ src/jscript.js
@@ -13,18 +13,27 @@ function hamClick() {
     console.log('click');
 }
 
-// Event Listner
-const burgers = document.querySelectorAll(".hamburgerMenu");
 
-for (let i = 0; i < burgers.length; i++) {
-    burgers[i].addEventListener("mouseover", function() {
-        setHamCol(red);
-        console.log('hover!!');
-    });
-}
 
-for (let i = 0; i < burgers.length; i++) {
-    burgers[i].addEventListener("click", function() {
-    console.log("click worked");
-  });
-}
+
+    const burgers = document.querySelectorAll(".hamburgerMenu");
+    
+    // for (let i = 0; i < burgers.length; i++) {
+    //     burgers[i].addEventListener("mouseover", function() {
+    //         setHamCol('red');
+    //         console.log(this);
+    //     });
+    // }
+
+    // for (let i = 0; i < burgers.length; i++) {
+    //     burgers[i].addEventListener("mouseout", function() {
+    //         setHamCol('blue');
+    //     });
+    // }
+    
+    for (let i = 0; i < burgers.length; i++) {
+        burgers[i].addEventListener("click", function() {
+            hamClick();
+            console.log(this);
+        });
+    }
