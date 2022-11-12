2efff656dec96dc0c6c1a33c6b4fed2469fadafd
Erol57 <113712076+Erol57@users.noreply.github.com>
2022-11-12 17:17:19 +0100

    bro

--- src/jscript.js
+++ src/jscript.js
@@ -1,14 +1,16 @@
 function setHamCol(color) {
-    document.getElementById('hamStripe1').style.backgroundColor = color;
-    document.getElementById('hamStripe2').style.backgroundColor = color;
-    document.getElementById('hamStripe3').style.backgroundColor = color;
+    //document.getElementById('hamStripe1').style.backgroundColor = color;
+    //document.getElementById('hamStripe2').style.backgroundColor = color;
+    //document.getElementById('hamStripe3').style.backgroundColor = color;
 }
 
 function setPlusCol (color) {
-    document.getElementById('vertical').style.backgroundColor = color;
-    document.getElementById('horizontal').style.backgroundColor = color;
+    //document.getElementById('vertical').style.backgroundColor = color;
+    //document.getElementById('horizontal').style.backgroundColor = color;
 }
 
 function hamClick() {
-    console.log('click');
+    //console.log('click');
 }
+
+
