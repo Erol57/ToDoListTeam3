c37b55a12ce836c442807beaaf8bc5bf9e7b4b1c
Trygve Horn <trygvehorn@gmail.com>
2022-11-14 11:42:44 +0100

    ss

--- src/jscript.js
+++ src/jscript.js
@@ -1,36 +1,36 @@
 function setHamCol(hamburgerID, color) {
-    document.getElementById(hamburgerID+'.1').style.backgroundColor = color;
-    document.getElementById(hamburgerID+'.2').style.backgroundColor = color;
-    document.getElementById(hamburgerID+'.3').style.backgroundColor = color;
-    console.log(hamburgerID);
-    console.log(document.getElementById(hamburgerID).childElementCount); 
+  document.getElementById(hamburgerID + ".1").style.backgroundColor = color;
+  document.getElementById(hamburgerID + ".2").style.backgroundColor = color;
+  document.getElementById(hamburgerID + ".3").style.backgroundColor = color;
+  console.log(hamburgerID);
+  console.log(document.getElementById(hamburgerID).childElementCount);
 }
 
-function setPlusCol (color) {
-    document.getElementById('vertical').style.backgroundColor = color;
-    document.getElementById('horizontal').style.backgroundColor = color;
+function setPlusCol(color) {
+  document.getElementById("vertical").style.backgroundColor = color;
+  document.getElementById("horizontal").style.backgroundColor = color;
 }
 
 function hamClick() {
-    console.log('click');
+  console.log("click");
 }
-    const burgers = document.querySelectorAll(".hamburgerMenu");
-    
-    for (let i = 0; i < burgers.length; i++) {
-        burgers[i].addEventListener("mouseover", function() {
-            setHamCol(this.id, 'red');
-        });
-    }
+const burgers = document.querySelectorAll(".hamburgerMenu");
 
-    for (let i = 0; i < burgers.length; i++) {
-        burgers[i].addEventListener("mouseout", function() {
-            setHamCol(this.id, 'blue');
-        });
-    }
-    
-    for (let i = 0; i < burgers.length; i++) {
-        burgers[i].addEventListener("click", function() {
-            hamClick();
-            console.log(this);
-        });
-    }
+for (let i = 0; i < burgers.length; i++) {
+  burgers[i].addEventListener("mouseover", function () {
+    setHamCol(this.id, "red");
+  });
+}
+
+for (let i = 0; i < burgers.length; i++) {
+  burgers[i].addEventListener("mouseout", function () {
+    setHamCol(this.id, "blue");
+  });
+}
+
+for (let i = 0; i < burgers.length; i++) {
+  burgers[i].addEventListener("click", function () {
+    hamClick();
+    console.log(this);
+  });
+}
