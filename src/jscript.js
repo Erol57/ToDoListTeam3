f12e8b5081a72b4e9ad6b5782c9497816635b96f
Trygve Horn <trygvehorn@gmail.com>
2022-11-15 20:19:43 +0100

    ParentAdd Works

--- src/jscript.js
+++ src/jscript.js
@@ -21,17 +21,17 @@ for (let i = 0; i < burgers.length; i++) {
 // newTask Listner
 const taskPlusses = document.querySelectorAll(".addTask.plusSign");
 
-// for (let i = 0; i < taskPlusses.length; i++) {
-//   taskPlusses[i].addEventListener("mouseover", function () {
-//     setPlusCol(this.id, "black");
-//   });
-// }
+for (let i = 0; i < taskPlusses.length; i++) {
+  taskPlusses[i].addEventListener("mouseover", function () {
+    setPlusCol(this.id, "black");
+  });
+}
 
-// for (let i = 0; i < taskPlusses.length; i++) {
-//   taskPlusses[i].addEventListener("mouseout", function () {
-//     setPlusCol(this.id, "cornflowerblue");
-//   });
-// }
+for (let i = 0; i < taskPlusses.length; i++) {
+  taskPlusses[i].addEventListener("mouseout", function () {
+    setPlusCol(this.id, "cornflowerblue");
+  });
+}
 
 for (let i = 0; i < taskPlusses.length; i++) {
   taskPlusses[i].addEventListener("click", function () {
@@ -69,73 +69,77 @@ function hamClick() {
   console.log("click");
 }
 
+function addItem(taskName = prompt("Please enter your ToDo", "My ToDo")) {
+  let taskCounter = document.querySelectorAll(".task.cardGrid").length + 1;
+  console.log("counter = " + taskCounter);
+  let taskID = "task" + taskCounter;
+  console.log(taskID);
 
-// * My adapted code
-
-function addItem(itemname = prompt("Please enter your ToDo", "My ToDo")) {
   const newTaskNode = document.createElement("div");
-  newTaskNode.className = "gridContainer cardGrid taskParent";
-  newTaskNode.id = "task2"; //todo dynamically change for each addition
-      console.log(newTaskNode);
+  newTaskNode.className = "gridContainer cardGrid task";
+  newTaskNode.id = taskID;
+  console.log(newTaskNode);
   document.getElementById("bodyGrid").appendChild(newTaskNode);
-  //textnewTaskNode.className = "text";
 
-  //set class of li
-/*  newitem = document.getElementById("ToDoList").appendChild(newTaskNode);
-  newitem.className = "card";
-  //todo add task defining code to div
+  const newCheckBoxGrid = document.createElement("div");
+  newCheckBoxGrid.className = "gridContainer checkBoxGrid";
+  newCheckBoxGrid.id = taskID + "checkBoxGrid";
+  document.getElementById(taskID).appendChild(newCheckBoxGrid);
 
-  //Add i for complete
-  const newTaskNode_i_c = document.createElement("i");
-  newitem.appendChild(newTaskNode_i_c);
-  newitem_i_c = newitem.appendChild(newTaskNode_i_c);
-  newitem_i_c.className = "fa fa-circle-thin co";
+  const newCheckBox = document.createElement("INPUT");
+  newCheckBox.setAttribute("type", "CheckBox");
+  newCheckBox.setAttribute("name", "taskCheckBox");
+  newCheckBox.id = taskID + "checkbox";
+  document.getElementById(taskID + "checkBoxGrid").appendChild(newCheckBox);
 
-  //Add i for hambumen
-  const newTaskNode_i_h = document.createElement("i");
-  newitem.appendChild(newTaskNode_i_h);
-  newitem_i_h = newitem.appendChild(newTaskNode_i_h);
-  newitem_i_h.className = "fa fa-bars ham";
+  const newTaskZone = document.createElement("div");
+  newTaskZone.className = "gridContainer taskZone";
+  newTaskZone.id = taskID + "TaskZone";
+  newTaskZone.innerHTML = "<h3>" + taskName;
+  document.getElementById(taskID).appendChild(newTaskZone);
 
-  //<p class="text">Buy Coffee</p>
-  const newTaskNode_i_p = document.createElement("p");
-  newitem.appendChild(newTaskNode_i_p);
-  newitem_i_p = newitem.appendChild(newTaskNode_i_p);
-  newitem_i_p.className = "text";
-  const textnewTaskNode = document.createTextnewTaskNode(itemname);
-  newitem_i_p.appendChild(textnewTaskNode); */
+  const newPlusSignBuffer = document.createElement("div");
+  newPlusSignBuffer.className = "gridContainer plusSignBuffer";
+  newPlusSignBuffer.id = taskID + "plusSignBuffer";
+  document.getElementById(taskID).appendChild(newPlusSignBuffer);
+
+  const newPlusSign = document.createElement("div");
+  newPlusSign.className = "gridContainer addTask plusSign";
+  newPlusSign.id = taskID + "plusSign";
+  document.getElementById(taskID + "plusSignBuffer").appendChild(newPlusSign);
+
+  const newVertical = document.createElement("div");
+  newVertical.className = "addSubTask plusLine vertical";
+  newVertical.id = taskID + "plusVertical";
+  document.getElementById(taskID + "plusSign").appendChild(newVertical);
+
+  const newHorizontal = document.createElement("div");
+  newHorizontal.className = "addSubTask plusLine horizontal";
+  newHorizontal.id = taskID + "plusHorizontal";
+  document.getElementById(taskID + "plusSign").appendChild(newHorizontal);
+
+  const newHamburgerMenuBuffer = document.createElement("div");
+  newHamburgerMenuBuffer.className = "gridContainer hamburgerMenuBuffer";
+  newHamburgerMenuBuffer.id = taskID + "HamburgerMenuBuffer";
+  document.getElementById(taskID).appendChild(newHamburgerMenuBuffer);
+
+  const newHamburgerMenu = document.createElement("div");
+  newHamburgerMenu.className = "gridContainer hamburgerMenu";
+  newHamburgerMenu.id = taskID + "HamburgerMenu";
+  document.getElementById(taskID + "HamburgerMenuBuffer").appendChild(newHamburgerMenu);
+
+  const newHamStripe1 = document.createElement("div");
+  newHamStripe1.className = "hamburgerStripe hamStripe1";
+  newHamStripe1.id = taskID + "HamStripe1";
+  document.getElementById(taskID + "HamburgerMenu").appendChild(newHamStripe1);
+
+  const newHamStripe2 = document.createElement("div");
+  newHamStripe2.className = "hamburgerStripe hamStripe2";
+  newHamStripe2.id = taskID + "HamStripe2";
+  document.getElementById(taskID + "HamburgerMenu").appendChild(newHamStripe2);
+
+  const newHamStripe3 = document.createElement("div");
+  newHamStripe3.className = "hamburgerStripe hamStripe3";
+  newHamStripe3.id = taskID + "HamStripe3";
+  document.getElementById(taskID + "HamburgerMenu").appendChild(newHamStripe3);
 }
-
-// ! copy of Erol Code
-
-// function addItem(itemname = prompt("Please enter your ToDo", "My ToDo")) {
-//   const node = document.createElement("li");
-
-
-//   document.getElementById("ToDoList").appendChild(node);
-//   //textnode.className = "text";
-
-//   //set class of li
-//   newitem = document.getElementById("ToDoList").appendChild(node);
-//   newitem.className = "card";
-
-//   //Add i for complete
-//   const node_i_c = document.createElement("i");
-//   newitem.appendChild(node_i_c);
-//   newitem_i_c = newitem.appendChild(node_i_c);
-//   newitem_i_c.className = "fa fa-circle-thin co";
-
-//   //Add i for hambumen
-//   const node_i_h = document.createElement("i");
-//   newitem.appendChild(node_i_h);
-//   newitem_i_h = newitem.appendChild(node_i_h);
-//   newitem_i_h.className = "fa fa-bars ham";
-
-//   //<p class="text">Buy Coffee</p>
-//   const node_i_p = document.createElement("p");
-//   newitem.appendChild(node_i_p);
-//   newitem_i_p = newitem.appendChild(node_i_p);
-//   newitem_i_p.className = "text";
-//   const textnode = document.createTextNode(itemname);
-//   newitem_i_p.appendChild(textnode);
-// }
