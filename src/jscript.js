62d394f1708ce6c17e3e6ca3f332db524d93f150
Trygve Horn <trygvehorn@gmail.com>
2022-11-15 08:46:48 +0100

    ss

--- src/jscript.js
+++ src/jscript.js
@@ -1,17 +1,3 @@
-function setHamCol(hamburgerID, color) {
-  document.getElementById(hamburgerID + ".1").style.backgroundColor = color;
-  document.getElementById(hamburgerID + ".2").style.backgroundColor = color;
-  document.getElementById(hamburgerID + ".3").style.backgroundColor = color;
-}
-
-function setPlusCol(plusID, color) {
-  document.getElementById(plusID + "Vertical").style.backgroundColor = color;
-  document.getElementById(plusID + "Horizontal").style.backgroundColor = color;
-}
-
-function hamClick() {
-  console.log("click");
-}
 
 // Hamburger Menu Listner
 const burgers = document.querySelectorAll(".hamburgerMenu");
@@ -48,6 +34,12 @@ for (let i = 0; i < taskPlusses.length; i++) {
   });
 }
 
+for (let i = 0; i < taskPlusses.length; i++) {
+  taskPlusses[i].addEventListener("click", function () {
+    addItem ();
+  });
+}
+
 // newSubTask Listner
 const plusses = document.querySelectorAll(".addSubTask");
 
@@ -62,3 +54,56 @@ for (let i = 0; i < plusses.length; i++) {
     setPlusCol(this.id, "orangered");
   });
 }
+
+function setHamCol(hamburgerID, color) {
+  document.getElementById(hamburgerID + ".1").style.backgroundColor = color;
+  document.getElementById(hamburgerID + ".2").style.backgroundColor = color;
+  document.getElementById(hamburgerID + ".3").style.backgroundColor = color;
+}
+
+function setPlusCol(plusID, color) {
+  document.getElementById(plusID + "Vertical").style.backgroundColor = color;
+  document.getElementById(plusID + "Horizontal").style.backgroundColor = color;
+}
+
+function hamClick() {
+  console.log("click");
+}
+
+
+function addItem(itemname = prompt("Please enter your ToDo", "My ToDo")) {
+  //Add li in myList
+  const newTaskNode = document.createElement("div");
+  //const textnewTaskNode = document.createTextnewTaskNode(itemname);
+  //newTaskNode.appendChild(textnewTaskNode);
+  
+  
+  document.getElementById("bodyGrid").appendChild(newTaskNode);
+  //textnewTaskNode.className = "text";
+
+  
+  //set class of li
+  newitem = document.getElementById("ToDoList").appendChild(newTaskNode);
+  newitem.className = "card";
+
+  //Add i for complete
+  const newTaskNode_i_c = document.createElement("i");
+  newitem.appendChild(newTaskNode_i_c);
+  newitem_i_c = newitem.appendChild(newTaskNode_i_c);
+  newitem_i_c.className = "fa fa-circle-thin co";
+
+  //Add i for hambumen
+  const newTaskNode_i_h = document.createElement("i");
+  newitem.appendChild(newTaskNode_i_h);
+  newitem_i_h = newitem.appendChild(newTaskNode_i_h);
+  newitem_i_h.className = "fa fa-bars ham";
+
+  //<p class="text">Buy Coffee</p>
+  const newTaskNode_i_p = document.createElement("p");
+  newitem.appendChild(newTaskNode_i_p);
+  newitem_i_p = newitem.appendChild(newTaskNode_i_p);
+  newitem_i_p.className = "text";
+  const textnewTaskNode = document.createTextnewTaskNode(itemname);
+  newitem_i_p.appendChild(textnewTaskNode);
+}
+*/
