b87d0037488632588a7eb420fa8cd238abf2e483
Erol57 <113712076+Erol57@users.noreply.github.com>
2022-11-30 20:54:56 +0100

    Erol_branch_footer

--- src/jscript.js
+++ src/jscript.js
@@ -133,17 +133,17 @@ function addMainTask(taskName = prompt("Please enter your ToDo", "My ToDo")) {
                               <div class="removeTask plusLine horizontal" id="${taskID}plusHorizontal"></div>
                           </div>
                       </div>
-                      <div class="gridContainer hamburgerMenuBuffer" id="${taskID}HamburgerMenuBuffer">
+                      <!-- <div class="gridContainer hamburgerMenuBuffer" id="${taskID}HamburgerMenuBuffer">
                           <div class="gridContainer hamburgerMenu" id="${taskID}HamburgerMenu">
                               <div class="hamburgerStripe hamburgerStripe1" id="${taskID}hamburgerStripe1"></div>
                               <div class="hamburgerStripe hamburgerStripe2" id="${taskID}hamburgerStripe2"></div>
                               <div class="hamburgerStripe hamburgerStripe3" id="${taskID}hamburgerStripe3"></div>
                           </div>
-                      </div>
+                      </div> -->
                     </section>`
 document.querySelector("#bodyGrid").insertAdjacentHTML("beforeend", newToDoHTML);
 removeTaskListener();
-hamburgerListener();
+//hamburgerListener();
 checkboxListener();
 renameTodoListener();
 getAllopenTasks();
@@ -221,9 +221,10 @@ function reorderAllTaskID() {
   }
 
   removeTaskListener();
-  hamburgerListener();
+  //hamburgerListener();
   checkboxListener();
   renameTodoListener();
+  getAllopenTasks();
 }
 
 //###### Start: Display clock on footer left side#######
@@ -251,6 +252,7 @@ function display_ct() {
 window.addEventListener('load', getAllopenTasks());
 function getAllopenTasks() {
   let allTasks = document.getElementsByClassName("gridContainer cardGrid task");
+  console.log(allTasks);
   if (allTasks.length == 0) {
     document.getElementById("ot").innerHTML = '';
   } else {
